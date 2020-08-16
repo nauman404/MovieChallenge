4eeb98abb4317ea18afc4e59335c33419b9de1a5
Nauman Ashraf <nauman.ashraf02@gmail.com>
2020-08-16 22:08:23 +0500

    Main activity renamed to movie activity

--- app/src/main/java/com/nauman404/moviechallenge/di/builder/FragmentBuilder.kt
+++ app/src/main/java/com/nauman404/moviechallenge/di/builder/FragmentBuilder.kt
@@ -5,4 +5,5 @@ import dagger.Module
 @Module
 abstract class FragmentBuilder {
 
+
 }
--- app/src/main/java/com/nauman404/moviechallenge/ui/movies/MovieActivity.kt
+++ app/src/main/java/com/nauman404/moviechallenge/ui/movies/MovieActivity.kt
@@ -1,9 +1,10 @@
-package com.nauman404.moviechallenge
+package com.nauman404.moviechallenge.ui.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nauman404.moviechallenge.R
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
